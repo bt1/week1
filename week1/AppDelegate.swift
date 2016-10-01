fba0534829828d65ca25f4c6d90b4a2b2e1b8c97
bt1 <benny@squarevibe.com>
2016-10-01 11:49:26 -0400

    created xcode project, added name change functions

--- week1/AppDelegate.swift
+++ week1/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Initiate frameworks or APIs
+        
+        print("hello world")
         return true
     }
 
@@ -27,6 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("goodbye")
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
--- week1/ViewController.swift
+++ week1/ViewController.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    var nameArray: NSArray = ["benny", "evelina", "aylon", "jason", "suzy", "alvin"]
+    var counter: NSInteger = 0
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        self.nameLabel.textColor = UIColor.blueColor()
+        
+        
+        // counter: tells us which index we're currently at, starts at 0
+        // increment 1 to the counter every time button pressed
+        // update label w/ new name
+        
+        
+        
+        //fix: add a if statement to check if index is > 5, if so we're 0
+        print(self.counter)
+        if (self.counter >= 5){
+            self.counter = 0;
+        }
+        else{
+            self.counter += 1
+        }
+        print(self.nameArray[counter])
+        self.nameLabel.text = self.nameArray[counter] as! String
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUppercaseName("Give me my name!")
+        showNames(nameArray)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +64,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setUppercaseName(statement: String){
+        print(statement)
+        //Change string to uppercase
+        self.nameLabel.text = self.nameArray[0] as! String
+        self.nameLabel.text = self.nameLabel.text?.uppercaseString
+        
+    }
+    
+    
+    
+    
+    func showNames(nameArray: NSArray){
+        // Pass in the name array and loop through each of them to display on UI
+        
+        for (var i = 0; i < nameArray.count; i++){
+            //code
+            
+            if (nameArray[i].isEqual("benny")){
+                print("that's me!")
+            }
+            else{
+                print ("that's \(nameArray[i])")
+            }
+            
+            //print(nameArray[i])
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
 
