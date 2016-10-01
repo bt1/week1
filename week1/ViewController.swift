dcb93c1049883a75331aa73548c9b57135c60002
bt1 <benny@squarevibe.com>
2016-10-01 09:45:23 -0400

    Initial Commit

---
+++ week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by local-admin on 10/1/16.
+//  Copyright © 2016 The Austin Space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
